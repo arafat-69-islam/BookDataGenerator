3fba5d1cf828b9d74edf4154d5b9ae2c7050da26
arafat-69-islam <1001221@daffodil.ac>
2025-08-07 00:50:20 +0600

    Delete wwwroot directory

--- wwwroot/js/export-handler.js
+++
@@ -1,7 +0,0 @@
-export async function exportBooksToCsv(params) {
-    const url = new URL('/api/export/csv', window.location.origin);
-    Object.entries(params).forEach(([key, value]) => {
-        url.searchParams.append(key, value);
-    });
-    window.location.href = url;
-}
