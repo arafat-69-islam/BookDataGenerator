c99bb79d442f6287937e427df4fbaf2d07f66557
arafat-69-islam <1001221@daffodil.ac>
2025-08-07 00:49:15 +0600

    Delete Models directory

--- Models/Review.cs
+++
@@ -1,9 +0,0 @@
-namespace BookDataGenerator.Models
-{
-    public class Review
-    {
-        public string Text { get; set; } = string.Empty;
-        public string Author { get; set; } = string.Empty;
-        public string Company { get; set; } = string.Empty;
-    }
-}
